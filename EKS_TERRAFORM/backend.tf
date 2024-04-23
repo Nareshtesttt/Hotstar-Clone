9cd3587387094a294a875df73647f4c5de18764c
rayabarapunaresh <43800475+rayabarapunaresh@users.noreply.github.com>
2024-04-23 15:21:47 +0530

    Update backend.tf

--- EKS_TERRAFORM/backend.tf
+++ EKS_TERRAFORM/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
-    bucket = "wworksapace" # Replace with your actual S3 bucket name
+    bucket = "backendsai" # Replace with your actual S3 bucket name
     key    = "EKS/terraform.tfstate"
-    region = "us-east-1"
+    region = "us-east-2"
   }
 }
